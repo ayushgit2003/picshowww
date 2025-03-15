ec51e7c7ba7cfd99658977d0d439e3b21b23686f
ayushgit2003 <ayush123tripathi.mt@gmail.com>
2025-03-16 02:46:22 +0530

    ci-cd

--- lib/data/network/base_api_services.dart
+++ lib/data/network/base_api_services.dart
@@ -2,4 +2,5 @@
 abstract class BaseApiServices {
   Future<List<T>> getApi<T>(String url, T Function(Map<String, dynamic>) fromJson);
   // Future<List<T>> getAp<T>(String url, T Function(Map<String, dynamic>) fromJson);
+ // Future<List<T>> getAp<T>(String url, T Function(Map<String, dynamic>) fromJson);
 }
